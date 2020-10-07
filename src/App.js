144fb0d7ec8daa3d89b5cc7cfb92d673f87b4755
Justin Gardner <the1jscott@gmail.com>
2020-10-07 15:25:40 -0500

    "created clickHandler"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import './components/MemoryCard.jsx';
 import MemoryCard from './components/MemoryCard.jsx';
 
 function App() {
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import  './MemoryCard.css';
 
 class MemoryCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { isFlipped: false };
+    }
+    clickHandler() {
+        this.setState({ isFlipped: !this.state.isFlipped });
+    }
     render() {
+        let memoryCardInnerClass = 'MemoryCardInner'
+        if (this.state.isFlipped) {
+            memoryCardInnerClass += ' flipped '
+        }
         return (
-            <div className='MemoryCard'>
-                <img className='DCLogo' src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'></img>
+            <div className='MemoryCard' onClick={ this.clickHandler.bind(this) }>
+                <div className={memoryCardInnerClass}>
+                    <div className='MemoryCardBack'>
+                        <img className="DCLogo" src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png' alt='DigitalCrafts Logo' />
+                    </div>
+                    <div className='MemoryCardFront'>
+                        ∆
+                    </div>
+                </div>
             </div>
         );
     }
