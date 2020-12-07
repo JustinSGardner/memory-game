3904768b36b39928a369bd61713428e46a86b7ca
Justin Gardner <the1jscott@gmail.com>
2020-12-07 09:46:23 -0600

    updated personal Title

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Memory Game</h1>
-          <h3 className="Subtitle">Match cards to win</h3>
+        <h1>Hello Preston</h1>
+          <h3 className="Subtitle">Match cards to win!</h3>
       </header>
         <div>
           <MemoryCard />
